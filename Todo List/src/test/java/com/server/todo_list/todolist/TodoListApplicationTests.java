bcbbe714d9652fdda6883028c04b886e51782788
Mohammed Ashraf <152704078+Mohammed-Zwam@users.noreply.github.com>
2026-02-20 16:51:09 +0200

    Todo List Project Init

---
+++ Todo List/src/test/java/com/server/todo_list/todolist/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.todo_list.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
